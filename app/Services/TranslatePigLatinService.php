82c27821818e6a4cf408931c666af468fface282
Jan Šulavík <jan.sulavik@seznam.cz>
2023-08-06 12:36:25 +0200

    Basic Pig Latin translator implementation

--- app/Services/TranslatePigLatinService.php
+++ app/Services/TranslatePigLatinService.php
@@ -5,15 +5,40 @@ namespace App\Services;
 class TranslatePigLatinService
 {
 
+    private array $consonants = [
+        'b', 'c', 'd', 'f',
+        'g', 'h', 'j', 'k',
+        'l', 'm', 'n', 'p',
+        'q', 'r', 's', 't',
+        'v', 'w', 'x', 'y',
+        'z',
+    ];
+
+    private array $vowels = [
+      'a', 'e', 'i', 'o', 'u',
+    ];
+
     public function __construct(){
 
     }
 
     public function translate(string $translationString): string
     {
-        $newString = $translationString . 'foo';
+        $consonantCluster = $rest = '';
+        $translationStringLen = strlen($translationString);
 
-        return $newString;
+        for ($i = 0; $i < $translationStringLen; $i++) {
+            $char = $translationString[$i];
+
+            if (in_array($char, $this->consonants, true)) {
+                $consonantCluster .= $char;
+                $rest = substr($translationString, $i+1);
+            } else {
+                break;
+            }
+        }
+
+        return $rest . $consonantCluster . 'ay';
     }
 
 }

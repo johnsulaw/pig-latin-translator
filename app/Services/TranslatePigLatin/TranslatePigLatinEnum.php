dcead7bc12d8b894d7286995e46ca31b586381f8
Jan Šulavík <jan.sulavik@seznam.cz>
2023-08-09 09:16:14 +0200

    Use capital letters also

--- app/Services/TranslatePigLatin/TranslatePigLatinEnum.php
+++ app/Services/TranslatePigLatin/TranslatePigLatinEnum.php
@@ -13,11 +13,17 @@ class TranslatePigLatinEnum
         'l', 'm', 'n', 'p',
         'q', 'r', 's', 't',
         'v', 'w', 'x', 'y',
-        'z',
+        'z', 'B', 'C', 'D',
+        'F', 'G', 'H', 'J',
+        'K', 'L', 'M', 'N',
+        'P', 'Q', 'R', 'S',
+        'T', 'V', 'W', 'X',
+        'Y', 'Z',
     ];
 
     private const VOWELS = [
         'a', 'e', 'i', 'o', 'u',
+        'A', 'E', 'I', 'O', 'U',
     ];
 
     public static function getConsonants(): array
